52689e74e2cbe782d16d0df1830a719dabb59748
Magpiny <magpinyb@gmail.com>
2020-10-06 00:18:17 +0300

    timer addec

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Timer from './components/Timer';
 
 function App() {
   return (
     <div className="App">
       <p>Welcome react to my mobile</p>
+      <Timer />
     </div>
   );
 }
---
+++ src/components/Timer.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+/*
+
+const Timer = ()=>{
+    <div>
+    <p>Time is {currTime}</p>
+    </div>
+}
+
+*/
+function Timer(){
+ const [currTime , setTime] = useState(new Date().toLocaleString());
+
+ 
+  useEffect(()=>{
+     let tUpdate = setInterval(()=>{ setTime(new Date().toLocaleString())
+
+     }, 1000);
+     return ()=>clearInterval(tUpdate);
+ }, []);
+
+
+return(
+    <div>
+    <p>Time is {currTime}</p>
+    </div>
+    );
+}
+
+export default Timer;
+
